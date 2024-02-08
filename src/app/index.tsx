5f2cd2b081ea9a777f453d49cefd0213adc516cb
Luiz Silva <lalsilva@gmail.com>
2024-02-08 01:47:34 -0300

    Utilização do compontente FlatList para exibição das categorias.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,25 @@
-import { View } from "react-native"
+import { FlatList, View } from "react-native"
+
+import { CATEGORIES } from "@/utils/data/products"
+
 import { Header } from "@/components/header"
+import { CategoryButton } from "@/components/category-button"
 
 export default function Home() {
     return (
         <View className="flex-1">
             <Header title="Faça seu pedido" cartQuantityItems={5} />
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <CategoryButton title={item} />
+                )}
+                horizontal
+                className="max-h-10 mt-5"
+                contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+                showsHorizontalScrollIndicator={false}
+            />
         </View>
     )
 }
