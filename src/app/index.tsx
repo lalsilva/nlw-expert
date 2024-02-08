a65c08671f4d487cf06f25fc1293b9a7d8800334
Luiz Silva <lalsilva@gmail.com>
2024-02-08 01:57:24 -0300

    Funcionalidade de seleção -utilizando o onPress- do botão de categoria.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,8 +4,15 @@ import { CATEGORIES } from "@/utils/data/products"
 
 import { Header } from "@/components/header"
 import { CategoryButton } from "@/components/category-button"
+import { useState } from "react"
 
 export default function Home() {
+    const [categorySelected, setCategorySelected] = useState(CATEGORIES[0])
+
+    function handleCategorySelect(category: string) {
+        setCategorySelected(category)
+    }
+
     return (
         <View className="flex-1">
             <Header title="Faça seu pedido" cartQuantityItems={5} />
@@ -13,7 +20,11 @@ export default function Home() {
                 data={CATEGORIES}
                 keyExtractor={(item) => item}
                 renderItem={({ item }) => (
-                    <CategoryButton title={item} />
+                    <CategoryButton
+                        title={item}
+                        isSelected={item === categorySelected}
+                        onPress={() => handleCategorySelect(item)}
+                    />
                 )}
                 horizontal
                 className="max-h-10 mt-5"
