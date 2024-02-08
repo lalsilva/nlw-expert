0bda1efcf39e9b4e884e0152b6907a214f4fdff8
Luiz Silva <lalsilva@gmail.com>
2024-02-08 01:20:56 -0300

    Criando o componente Header.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native"
+import { View } from "react-native"
+import { Header } from "./header"
 
 export default function Home() {
     return (
-        <View>
-            <Text>Hello React Native!</Text>
+        <View className="flex-1">
+            <Header title="Faça seu pedido" cartQuantityItems={5} />
         </View>
     )
 }
